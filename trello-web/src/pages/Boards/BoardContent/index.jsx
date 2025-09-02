51d801fec32ba8ca6b5167b184ecc73a4dcd951f
congquynguyen296 <congquynguyen296@gmail.com>
2025-09-02 18:30:19 +0700

    Update: Refactor structure

---
+++ trello-web/src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import theme from "../../../theme";
+
+function BoardContent() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "primary.main",
+        width: "100%",
+        height: theme.trello.boardContentHeight,
+        display: "flex",
+        justifyContent: "flex",
+        alignItems: "center",
+      }}
+    >
+      Board content
+    </Box>
+  );
+}
+
+export default BoardContent;
