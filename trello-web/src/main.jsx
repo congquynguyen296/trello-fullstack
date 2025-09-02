8e992a6d2e11c3925973fd404d36ceff083238a8
congquynguyen296 <congquynguyen296@gmail.com>
2025-09-02 16:44:20 +0700

    Update: Dart Light System mode

--- trello-web/src/main.jsx
+++ trello-web/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+import theme from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </React.StrictMode>
 );
---
+++ trello-web/src/theme.js
@@ -0,0 +1,92 @@
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { blue, deepOrange, deepPurple, orange } from "@mui/material/colors";
+
+const APP_BAR_HEIGHT = "58px";
+const BOARD_BAR_HEIGHT = "60px";
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`;
+const COLUMN_HEADER_HEIGHT = "50px";
+const COLUMN_FOOTER_HEIGHT = "56px";
+
+// Create a theme instance.
+const theme = extendTheme({
+  trello: {
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    columnHeaderHeight: COLUMN_HEADER_HEIGHT,
+    columnFooterHeight: COLUMN_FOOTER_HEIGHT,
+  },
+
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: blue,
+        secondary: deepOrange,
+      },
+      spacing: (factor) => `${0.25 * factor}rem`,
+    },
+    dark: {
+      palette: {
+        primary: deepPurple,
+        secondary: orange,
+      },
+      spacing: (factor) => `${0.25 * factor}rem`,
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          "*::-webkit-scrollbar": {
+            width: "8px",
+            height: "8px",
+          },
+          "*::-webkit-scrollbar-thumb": {
+            backgroundColor: "#dcdde1",
+            borderRadius: "8px",
+          },
+          "*::-webkit-scrollbar-thumb:hover": {
+            backgroundColor: "white",
+            borderRadius: "8px",
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+          borderWidth: "0.5px",
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          fontSize: "0.875rem",
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          "&.MuiTypography-body1": {
+            fontSize: "0.875rem",
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          fontSize: "0.875rem",
+          "& fieldset": { borderWidth: "0.5px !important" },
+          "&:hover fieldset": { borderWidth: "2px !important" },
+          "&:Mui-focused fieldset": { borderWidth: "2px !important" },
+        },
+      },
+    },
+  },
+});
+
+export default theme;
